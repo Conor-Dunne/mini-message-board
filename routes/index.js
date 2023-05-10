0ecafb6abd1d24d2403389db6b6c122cf1e6e04a
Conor Dunne <conordunne@outlook.ie>
2023-05-10 06:05:18 +0000

    Add btn

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const moment = require('moment');
 
 const messages = [
   {
+    text: "Thank you for visting my project!",
+    user: "Conor",
+    added: moment().format('MMMM Do YYYY, h:mm:ss a')
+  },
+  {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: moment().format('MMMM Do YYYY, h:mm:ss a')
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: moment().format('MMMM Do YYYY, h:mm:ss a')
   }
 ];
 
@@ -23,5 +29,12 @@ router.get('/', function(req, res, next) {
 router.get('/new', function(req,res,next) {
   res.render("form",{ title: 'Add Comment'} );
 })
+
+// Post message
+router.post('/new', (req, res, next) => {
+  console.log(req.body)
+  messages.unshift({text: req.body.comment, user: req.body.name, added: moment().format('MMMM Do YYYY, h:mm:ss a')});
+  res.redirect('/');
+});
 
 module.exports = router;
